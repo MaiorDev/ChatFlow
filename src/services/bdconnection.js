9a34b233bfdae0ca7048e3513a68ba0f9a5a66e3
Maior Dev <marianojeronimobautista@gmail.com>
2025-07-02 17:51:13 -0600

    Update bdconnection.js

--- src/services/bdconnection.js
+++ src/services/bdconnection.js
@@ -3,7 +3,7 @@ const { Pool } = pkg;
 
 // Configuración para Railway (usando DATABASE_URL)
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL_RAILWAY,
   // Opcional: configuración adicional para SSL si es necesario
   ssl: {
     rejectUnauthorized: false,
