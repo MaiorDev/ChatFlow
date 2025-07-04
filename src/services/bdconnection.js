a94f873859480c7b1cc99936f6a31798984d54ea
Maior <marianojeronimobautista@gmail.com>
2025-07-04 11:44:07 -0600

    fix bd

--- src/services/bdconnection.js
+++ src/services/bdconnection.js
@@ -1,14 +1,11 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-// Configuración para Railway (usando DATABASE_URL)
 const pool = new Pool({
-  user: process.env.DB_NAME || "postgres",
-  password: process.env.DB_PASSWORD || "12345678",
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 6599,
-  database: process.env.DB_DATABASE || "funkyvaso",
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necesario para Railway
+  },
 });
 
 export default pool;
